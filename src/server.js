9fab338f767d1a67e6a3b1cd43cd9ea767c28a93
Oleksii Cherep <alex1998120850@gmail.com>
2026-03-23 19:48:21 +0200

    feat: add CRUD operations for notes model

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import notesRoutes from './routes/notesRoutes.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -15,6 +16,7 @@ export const setupServer = async () => {
   app.use(logger);
   app.use(cors());
   app.use(express.json());
+  app.use(notesRoutes);
   app.use(notFoundHandler);
   app.use(errorHandler);
 
