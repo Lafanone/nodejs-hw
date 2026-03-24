7f3d3fc940e42fb7978079fd775cc4fd42ec8c83
Oleksii Cherep <alex1998120850@gmail.com>
2026-03-24 19:14:24 +0200

    fix: update mongoose options and add HttpError check in errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,11 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
   const status = err.status || 500;
   res.status(status).json({
     message: err.message || 'Something went wrong',
