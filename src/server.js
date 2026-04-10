ccf08313f125d00cd17c572735f5d3134fb2b8a0
Oleksii Cherep <alex1998120850@gmail.com>
2026-04-10 17:43:15 +0300

    fix: register authRoutes without prefix in server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ export const setupServer = async () => {
   app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
-  app.use('/auth', authRoutes);
+  app.use(authRoutes);
   app.use(notesRoutes);
   app.use(errors());
   app.use(notFoundHandler);
